eef32789ff3374d8a0fb9c13a8cb17ec0bff681e
kevinccbsg <kevinccbsg@gmail.com>
2023-10-02 17:20:52 +0200

    feat: add basic structure and components to display in nelify

---
+++ components/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    header
+    <img src="~/assets/pumas.png" alt="Pumas logo" />
+  </header>
+</template>
+<style lang="scss" scoped>
+img {
+  width: 100%;
+  max-width: 120px;
+}
+</style>
