0fe3e5e5b74747c2aa10ebaf8d90bbc13f3dcbb2
kevinccbsg <kevinccbsg@gmail.com>
2023-10-04 11:20:37 +0200

    feat: add calendar and club pages

--- pages/calendario.vue
+++ pages/calendario.vue
@@ -1,6 +1,6 @@
 <template>
   <HomeSectionsCalendar/>
-  <section>
+  <section class="ui container">
     <h1>Calendario</h1>
     <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%230B8043&ctz=Europe%2FMadrid&showNav=0&showDate=1&showPrint=0&showTabs=1&showCalendars=0&src=a2V2aW5jY2JzZ0BnbWFpbC5jb20&src=dHVocXRjMHRuYWMwbmkwa3Vxbms2MGo2ZGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=MTdidDU0YTFnM2t0cmVxaGNpcnVmdDVwY3NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%23F09300&color=%23D50000&color=%2333B679" style="border:solid 1px #777" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
   </section>
--- pages/club.vue
+++ pages/club.vue
@@ -1,6 +1,20 @@
 <template>
-  <HomeSectionsBio />
-  <section>
-    <h1>Nosotros</h1>
+  <section class="ui container">
+    <div class="noavailable-container">
+      <h1>En construcción <i class="wrench icon"></i></h1>
+    </div>
   </section>
 </template>
+<style lang="scss" scoped>
+.noavailable-container {
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    text-align: center;
+    font-size: 4rem;
+    margin: 0;
+  }
+}
+</style>
